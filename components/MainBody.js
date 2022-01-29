524456e2bf5a6aca4d574be093df799ca8f995b8
My Name <ben.d.maryland@gmail.com>
2022-01-29 18:02:56 -0500

    removed main body

--- components/MainBody.js
+++ components/MainBody.js
@@ -14,12 +14,12 @@ function voicegrab(e){
     return (
       <View>
 
-        <Text>Main Body goes Here</Text>
+        {/* <Text>Main Body goes Here</Text>
         <Button onPress={e=>voicegrab(e)} title='Click Here'></Button>
         <Text>{name}, name</Text>
             <TextInput 
                 placeholder="Type here to translate!"
-            onChangeText={text => setName(text)} />
+            onChangeText={text => setName(text)} /> */}
             {/* <Image source={{
                 width: 200,
                 height: 300,
